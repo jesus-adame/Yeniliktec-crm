164c1d7751857b4c461717afb03acf7dddb1b685
Jesus Adame <jesus.adame@ancient.mx>
2021-07-25 19:13:38 -0500

    fix: inertia array_merge error

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -36,6 +36,8 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request)
     {
-        //
+        return array_merge(parent::share($request), [
+            //
+        ]);
     }
 }
